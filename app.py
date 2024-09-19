91decf16538536e67b60608709e18d070201cc8a
mmtknk <sweatengineer@gmail.com>
2024-09-18 18:12:56 -0700

    app.py

--- app.py
+++ app.py
@@ -64,31 +64,7 @@ if author_name:
     filtered_data = filtered_data[filtered_data['authfull'].str.contains(author_name, case=False, na=False)]
 
 
-# Author Distribution by Country
-st.subheader("Author Distribution by Country")
-if not filtered_data.empty:
-    country_counts = filtered_data['cntry'].value_counts().reset_index()
-    country_counts.columns = ['cntry', 'count']
-    country_counts['cntry'] = country_counts['cntry'].str.upper()
-    
-    valid_iso3_codes = set([country.alpha_3 for country in pycountry.countries])
-    country_counts = country_counts[country_counts['cntry'].isin(valid_iso3_codes)]
-    
-    if not country_counts.empty:
-        country_counts['country_name'] = country_counts['cntry'].apply(lambda x: country_mapping.get(x.lower(), x))
-        
-        fig_map = px.choropleth(country_counts,
-                                locations="cntry",
-                                color="count",
-                                hover_name="country_name",
-                                locationmode="ISO-3",
-                                color_continuous_scale="Viridis",
-                                labels={"count": "Number of Authors"})
-        st.plotly_chart(fig_map)
-    else:
-        st.write("No valid country data to display on the map.")
-else:
-    st.write("No data to display.")
+
 
 
 
@@ -120,7 +96,31 @@ for i, (inst_name, count) in enumerate(top_institutions.items(), 1):
     st.write(f"{i}. {inst_name} ({count} authors)")
 
 
-
+# Author Distribution by Country
+st.subheader("Author Distribution by Country")
+if not filtered_data.empty:
+    country_counts = filtered_data['cntry'].value_counts().reset_index()
+    country_counts.columns = ['cntry', 'count']
+    country_counts['cntry'] = country_counts['cntry'].str.upper()
+    
+    valid_iso3_codes = set([country.alpha_3 for country in pycountry.countries])
+    country_counts = country_counts[country_counts['cntry'].isin(valid_iso3_codes)]
+    
+    if not country_counts.empty:
+        country_counts['country_name'] = country_counts['cntry'].apply(lambda x: country_mapping.get(x.lower(), x))
+        
+        fig_map = px.choropleth(country_counts,
+                                locations="cntry",
+                                color="count",
+                                hover_name="country_name",
+                                locationmode="ISO-3",
+                                color_continuous_scale="Viridis",
+                                labels={"count": "Number of Authors"})
+        st.plotly_chart(fig_map)
+    else:
+        st.write("No valid country data to display on the map.")
+else:
+    st.write("No data to display.")
 
 
 
